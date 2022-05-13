78ef97c353557ac6a8373b702af1d2cc822ffd7e
Juan Villacorta <juanvillacortac@gmail.com>
2022-05-13 00:51:36 -0400

    ...

--- src/routes/products/[productSlug].svelte
+++ src/routes/products/[productSlug].svelte
@@ -6,11 +6,14 @@
   import Markdown from 'svelte-markdown'
   import { tooltip } from '$lib/components/tooltip'
   import {
+    Add16,
     Checkbox16,
     ZoomFit16,
     ZoomIn16,
     ZoomOut16,
+    Subtract16,
   } from 'carbon-icons-svelte'
+  import { clamp } from '$lib/utils/math'
 
   export const load: Load = async ({ params, fetch, stuff }) => {
     const store = stuff.store
@@ -33,9 +36,8 @@
 <script lang="ts">
   export let product: Product
   let scale = 100
-  let border = true
-  const zoomIn = () => (scale = Math.max(10, Math.min(scale + 10, 200)))
-  const zoomOut = () => (scale = Math.max(10, Math.min(scale - 10, 200)))
+
+  let quantity = 1
 </script>
 
 <div
@@ -47,71 +49,20 @@
     class:lg:grid-cols-3={product.template && product.isTemplate}
   >
     {#if product.template && product.isTemplate}
-      <div
-        class="border rounded-lg h-full border-gray-300 w-auto overflow-hidden pointer-events-none  select-none  dark:border-gray-800"
-        style="aspect-ratio: 1/1"
-      >
+      <div class="flex h-full relative items-start">
         <div
-          class="flex h-full w-full items-center justify-center checkerboard"
-        >
-          <Preview template={product?.template} fitParent />
-        </div>
-      </div>
-      <!-- <div class="flex h-full w-full relative" style="aspect-ratio: 1/1"> -->
-      <!-- <div
-          class="border rounded h-full border-gray-300 w-full absolute overflow-auto checkerboard dark:border-gray-800"
+          class="border rounded-lg h-auto border-gray-300 w-full top-0 col-span-1 sticky overflow-hidden pointer-events-none  select-none  dark:border-gray-800"
+          style="aspect-ratio: 1/1"
         >
           <div
-            class="origin-top-left transition-transform duration-200"
-            style="transform: scale({scale / 100})"
+            class="flex h-full w-full items-center justify-center checkerboard"
           >
-            <Preview template={JSON.parse(product.template)} {border} />
+            <Preview template={product?.template} fitParent />
           </div>
         </div>
-        <div
-          class="flex space-x-1 right-1rem bottom-1rem absolute items-center"
-        >
-          <p class="font-bold text-xs pr-4">{scale}%</p>
-          <button
-            class="preview-button"
-            title="Zoom Out"
-            type="button"
-            use:tooltip
-            on:click={zoomOut}
-          >
-            <ZoomOut16 class="font-bold" />
-          </button>
-          <button
-            class="preview-button"
-            title="Reset zoom"
-            type="button"
-            use:tooltip
-            on:click={() => (scale = 100)}
-          >
-            <ZoomFit16 class="font-bold" />
-          </button>
-          <button
-            class="preview-button"
-            title="Zoom In"
-            type="button"
-            use:tooltip
-            on:click={zoomIn}
-          >
-            <ZoomIn16 class="font-bold" />
-          </button>
-          <button
-            class="preview-button"
-            type="button"
-            title="Toggle border"
-            use:tooltip
-            on:click={() => (border = !border)}
-          >
-            <Checkbox16 class="font-bold" />
-          </button>
-        </div> -->
-      <!-- </div> -->
+      </div>
     {/if}
-    <div class="flex flex-col space-y-4 w-full lg:col-span-2">
+    <div class="flex flex-col space-y-4 w-full">
       <div class="flex flex-col space-y-2 items-start">
         <h3 class="font-bold font-title text-black text-3xl dark:text-white">
           {product.name}
@@ -122,11 +73,61 @@
           >{product.storeCategory.name}</a
         >
       </div>
-      <p class="font-bold text-4xl">
+      <p class="font-bold text-black text-4xl lg:hidden dark:text-white">
         ${product.price.toLocaleString()} <span class="text-lg">/ unit</span>
       </p>
       <div class="prose-sm !w-full">
         <Markdown source={product.description || 'No description'} />
+      </div>
+    </div>
+    <div class="flex flex-col space-y-4 w-full lg:text-right">
+      <p class="font-bold text-black text-4xl <lg:hidden dark:text-white">
+        ${product.price.toLocaleString()} <span class="text-lg">/ unit</span>
+      </p>
+      <div class="flex <sm:hidden !text-xs lg:self-end">
+        <button
+          class="border rounded-l-full flex bg-light-600 border-gray-300 p-2 items-center dark:bg-gray-700  dark:border-gray-600"
+          on:click={() =>
+            (quantity = Math.max(product.minQuantity || 1, quantity - 1))}
+        >
+          <Subtract16 class="m-auto" />
+        </button>
+        <input
+          class="bg-white border-t border-b border-gray-300 border-l-0 border-r-0 text-xs text-center leading-tight py-2 px-3 w-6ch quantity dark:bg-gray-700 dark:border-gray-600 focus:outline-none focus:shadow-outline focus:z-10"
+          type="number"
+          min={Math.max(product.minQuantity || 1)}
+          bind:value={quantity}
+        />
+        <button
+          class="border rounded-r-full flex bg-light-600 border-gray-300 p-2 items-center dark:bg-gray-700  dark:border-gray-600"
+          on:click={() => quantity++}
+        >
+          <Add16 class="m-auto" />
+        </button>
+      </div>
+      <div class="flex flex-col space-y-4">
+        {#each product.modifiers as m}
+          <div class="flex flex-col space-y-2">
+            <h4 class="font-bold font-title text-black text-xl dark:text-white">
+              {m.name}
+            </h4>
+            {#if m.type === 'select'}
+              <select
+                class="bg-white border rounded border-gray-300 text-xs leading-tight w-full py-2 px-3 appearance-none dark:bg-gray-700 dark:border-gray-600 focus:outline-none focus:shadow-outline"
+              >
+                {#each m.items as i}
+                  <option value={i.id}
+                    >{i.name}&nbsp;&nbsp;&ndash;&nbsp;&nbsp;<strong
+                      >{i.cost < 0 ? '-' : ''}{!i.percentage
+                        ? '$'
+                        : ''}{Math.abs(i.cost)}{i.percentage ? '%' : ''}</strong
+                    ></option
+                  >
+                {/each}
+              </select>
+            {/if}
+          </div>
+        {/each}
       </div>
     </div>
   </div>
@@ -157,5 +158,13 @@
   :global(.dark) .checkerboard {
     --black-cell: rgba(55, 65, 81, 0.5);
     background-color: rgba(80, 80, 80, 0.2);
+  }
+  .quantity::-webkit-outer-spin-button,
+  .quantity::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+  .quantity {
+    -moz-appearance: textfield;
   }
 </style>
