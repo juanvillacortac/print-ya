17326f676297071adaad27e635508798d14abde8
Juan Manuel Villacorta Campos <37244056+juanvillacortac@users.noreply.github.com>
2022-05-18 10:34:32 -0400

    ...

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,93 +1,93 @@
-import { getUserDetails } from '$lib/db/users'
-import type { GetSession } from '@sveltejs/kit'
-import { handleSession } from 'svelte-kit-cookie-session'
-import cookie from 'cookie'
-import { appRoutes, getLayoutType, storeRoutes } from '$lib/utils/layout'
-import { getDefaultHost } from '$lib/utils/host'
-import { browser } from '$app/env'
+import { getUserDetails } from "$lib/db/users";
+import type { GetSession } from "@sveltejs/kit";
+import { handleSession } from "svelte-kit-cookie-session";
+import cookie from "cookie";
+import { appRoutes, getLayoutType, storeRoutes } from "$lib/utils/layout";
+import { getDefaultHost } from "$lib/utils/host";
+import { browser } from "$app/env";
 
 export const handle = handleSession(
   {
-    secret: process.env['VITE_PY_SECRET'] || 'secret',
+    secret: process.env["VITE_PY_SECRET"] || "secret",
     expires: 30,
     cookie: { secure: false },
   },
   async function ({ event, resolve }) {
-    let response: Response
+    let response: Response;
 
-    event.locals.layout = getLayoutType(event)
+    event.locals.layout = getLayoutType(event);
 
     // Set default locale if user preferred locale does not match
     try {
       if (event.locals.cookies) {
-        if (event.locals.cookies['kit.session']) {
-          const { userId } = await getUserDetails(event)
+        if (event.locals.cookies["kit.session"]) {
+          const { userId } = await getUserDetails(event);
           const newSession = {
             userId,
             expires: event.locals.session.data.expires,
-          }
+          };
 
           if (
             JSON.stringify(event.locals.session.data) !==
             JSON.stringify(newSession)
           ) {
-            event.locals.session.data = { ...newSession }
+            event.locals.session.data = { ...newSession };
           }
         }
       }
 
       const isAppPage =
-        event.locals.layout === 'app' &&
+        event.locals.layout === "app" &&
         (Boolean(appRoutes.find((url) => event.url.pathname.startsWith(url))) ||
-          event.url.pathname === '/')
+          event.url.pathname === "/");
 
       if (
         isAppPage &&
         !event.locals.session?.data?.userId &&
-        event.url.pathname !== '/login'
+        event.url.pathname !== "/login"
       ) {
         return Response.redirect(
-          `${
-            getDefaultHost() === 'localhost:3000' ? 'http://' : 'https://'
-          }${event.url.host === process.env.VERCEL_URL ? process.env.VERCEL_URL : getDefaultHost()}/login?callbackUrl=${encodeURIComponent(
-            event.url.pathname
-          )}`,
+          `${getDefaultHost() === "localhost:3000" ? "http://" : "https://"}${
+            event.url.host === process.env.VERCEL_URL
+              ? process.env.VERCEL_URL
+              : getDefaultHost()
+          }/login?callbackUrl=${encodeURIComponent(event.url.pathname)}`,
           303
-        )
+        );
       }
 
-    response = await resolve(event, {
-        ssr: event.locals.layout !== 'app',
-      })
+      response = await resolve(event, {
+        ssr: event.locals.layout !== "app",
+      });
 
-      if (event.locals.layout === 'store') {
+      if (event.locals.layout === "store") {
         response.headers.set(
-          'Cache-Control',
-          's-maxage=1, stale-while-revalidate'
-        )
+          "Cache-Control",
+          "s-maxage=1, stale-while-revalidate"
+        );
       }
     } catch (error) {
-      console.error(error)
+      console.error(error);
       response = await resolve(event, {
-        ssr: event.locals.layout !== 'app',
-      })
+        ssr: event.locals.layout !== "app",
+      });
       response.headers.append(
-        'Set-Cookie',
-        cookie.serialize('kit.session', '', {
-          path: '/',
-          expires: new Date('Thu, 01 Jan 1970 00:00:01 GMT'),
+        "Set-Cookie",
+        cookie.serialize("kit.session", "", {
+          path: "/",
+          expires: new Date("Thu, 01 Jan 1970 00:00:01 GMT"),
         })
-      )
+      );
     }
 
-    return response
+    return response;
   }
-)
+);
 
 export const getSession: GetSession = (event) => ({
   layout: event.locals.layout,
-  userAgent: event.request.headers.get('user-agent'),
+  userAgent: event.request.headers.get("user-agent"),
   host: event.url.host,
   fullHost: `${event.url.protocol}//${event.url.host}`,
   ...event.locals.session.data,
-})
+});
--- src/routes/stores/[slug]/__layout.svelte
+++ src/routes/stores/[slug]/__layout.svelte
@@ -1,39 +1,39 @@
 <script context="module" lang="ts">
-  import type { Load } from '@sveltejs/kit'
-  import { get } from '$lib/api'
-  import Image from '$lib/components/caravaggio/Image.svelte'
-  import { tooltip } from '$lib/components/tooltip'
-  import { Launch16 } from 'carbon-icons-svelte'
-  import type { Store } from '$lib/db'
+  import type { Load } from "@sveltejs/kit";
+  import { get } from "$lib/api";
+  import Image from "$lib/components/caravaggio/Image.svelte";
+  import { tooltip } from "$lib/components/tooltip";
+  import { Launch16 } from "carbon-icons-svelte";
+  import type { Store } from "$lib/db";
 
   export const load: Load = async ({ params, fetch, session }) => {
     const stuff = await get<{ store: Store }>(`/api/stores/${params.slug}`, {
       fetch,
-    })
+    });
     if (!stuff.store || stuff.store.userId !== session?.userId) {
       return {
         status: 404,
-      }
+      };
     }
 
     return {
       stuff,
-    }
-  }
+    };
+  };
 </script>
 
 <script lang="ts">
-  import { page } from '$app/stores'
-  import { invalidate } from '$app/navigation'
-  import { browser } from '$app/env'
-  import { getAbsoluteURL } from '$lib/utils/host'
+  import { page } from "$app/stores";
+  import { invalidate } from "$app/navigation";
+  import { browser } from "$app/env";
+  import { getAbsoluteURL } from "$lib/utils/host";
 
-  let store: Store
-  $: store = $page.stuff.store
+  let store: Store;
+  $: store = $page.stuff.store;
 
-  $: path = $page.url.pathname
+  $: path = $page.url.pathname;
   $: if (path && store && browser) {
-    invalidate(`/api/stores/${store.slug}`)
+    invalidate(`/api/stores/${store.slug}`);
   }
 </script>
 
@@ -41,7 +41,10 @@
   My Stores
 </h2>
 <div class="flex space-x-4 mb-6 items-center">
-  <a class="flex space-x-4 items-center" href="/stores/{store.slug}">
+  <a
+    class="flex <sm:space-y-4 sm:space-x-4 items-center <sm:flex-col"
+    href="/stores/{store.slug}"
+  >
     <div class="rounded bg-light-100 shadow p-1 dark:bg-gray-800">
       <Image
         src={store.logo}
@@ -57,7 +60,10 @@
   </a>
   <a
     class="border-transparent flex hover:border-current"
-    href={getAbsoluteURL({ subdomain: store.slug })}
+    href={getAbsoluteURL({
+      subdomain: store.customDomain ? store.slug : undefined,
+      host: store.customDomain,
+    })}
     target="__blank"
     title="Go to site"
     use:tooltip
