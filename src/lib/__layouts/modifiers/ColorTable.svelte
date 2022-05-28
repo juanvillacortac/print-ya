d1221181b71e89eebe3f057830d64f53132fd448
Juan Manuel Villacorta Campos <juanvillacortac@gmail.com>
2022-05-28 15:08:55 +0000

    ...

--- src/lib/__layouts/modifiers/ColorTable.svelte
+++ src/lib/__layouts/modifiers/ColorTable.svelte
@@ -96,7 +96,7 @@
               <input
                 type="text"
                 placeholder="Color HEX"
-                pattern="[a-fA-F\d]+"
+                pattern="#[a-fA-F\d]+"
                 class="bg-white border rounded border-gray-300 text-xs leading-tight w-full py-2 px-3 appearance-none dark:bg-gray-700 dark:border-gray-600 focus:outline-none focus:shadow-outline"
                 bind:value={i.name}
                 required
