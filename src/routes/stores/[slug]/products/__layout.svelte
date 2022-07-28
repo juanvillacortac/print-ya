821f6868dd4da06a0b2e4503b59d654afcd4842e
Juan Villacorta <juanvillacortac@gmail.com>
2022-07-28 04:27:25 -0400

    Products imports, products tags and much more

--- src/routes/stores/[slug]/products/__layout.svelte
+++ src/routes/stores/[slug]/products/__layout.svelte
@@ -1,18 +1,1 @@
-<script context="module" lang="ts">
-  import type { Load } from '@sveltejs/kit'
-  import trpc from '$lib/trpc/client'
-
-  export const load: Load = async ({ params, fetch, stuff }) => {
-    const data = await trpc(fetch).query('products:list', {
-      storeSlug: params.slug,
-    })
-    return {
-      stuff: {
-        ...stuff,
-        products: data,
-      },
-    }
-  }
-</script>
-
 <slot />
